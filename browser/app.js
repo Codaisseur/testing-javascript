ccadbb2935323f4e3baa6a8c4aff2fd06c9d09a3
rorykoehein <rory@intelworks.com>
2016-03-03 11:00:52 +0100

    show the car during tests with some shitty dom manipulation!

--- browser/app.js
+++ browser/app.js
@@ -2,6 +2,9 @@ function Car(){
     this.x = 10;
     this.y = 10;
 
+    createDOMNode(this);
+    go(this);
+
     this.drive = function( direction ){
         switch(direction) {
             case 'left':
@@ -17,5 +20,25 @@ function Car(){
                 this.y += 10;
                 break;
         }
-    }
+
+        this.element.style.left = this.x + "px";
+        this.element.style.top = this.y + "px";
+    };
+}
+
+function createDOMNode(car){
+    car.element = document.createElement('div');
+    car.element.style.position = "absolute";
+    car.element.style.left = car.x + "px";
+    car.element.style.top = car.y + "px";
+    car.element.style.width = 20 + "px";
+    car.element.style.height = 10 + "px";
+    car.element.style.backgroundColor = "red";
+    document.body.appendChild(car.element);
+}
+
+function go(car){
+    setInterval(function(){
+        car.drive("right");
+    }, 1000);
 }
